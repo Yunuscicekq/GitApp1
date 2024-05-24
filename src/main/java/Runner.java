8561ab810d461b309b0b4ced3a77108d2ed337cb
Yunus Cicek <yns2002cck@gmail.comgit config --global user.email>
2024-05-24 22:43:26 +0300

    v2

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -4,5 +4,8 @@ public class Runner {
         //versiyon 1
         System.out.println("hello GIT");
         System.out.println("git add. : staging areaya alir.");
+
+        //versiyon 2
+        System.out.println("git commit -m\"\" ile versiyon olusturulur.");
     }
 }
